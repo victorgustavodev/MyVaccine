cd8036a153ee23b60bd0e033ee6be0d282ef716f
yavtt <rafaelfigueiredo14nunes@gmail.com>
2025-02-25 14:22:14 -0300

    Navbar exclusiva para user

---
+++ components/navbaruser.php
@@ -0,0 +1,22 @@
+<nav class="px-[6%] h-[8%] flex justify-between items-center navbar text-[#100E3D] bg-white shadow-md">
+    <a href="../index.php"><img src="../assets/img/logo.png" alt="logo" class="w-[190px]" /></a>
+    <div class="flex flex-grow justify-center items-center gap-[64px] text-[16px]">
+        <a href="../posts/read-post.php" class="text-xs md:text-sm rounded-md">
+            Postos
+        </a>
+
+        <a href="../index.php" class="text-xs md:text-sm rounded-md">
+            Home
+        </a>
+    </div>
+    <ul class="flex gap-6 justify-center items-center">
+        <li>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOciWshHkTzFKgiKMJHUOW3OSQQ8Dv1A8ZWKkYZ2nKPgMmy7MOETlpjOp62raFIl-IOt0&usqp=CAU"
+                class="w-10 p-1 2xl:p-[0px] rounded-full" alt="">
+        </li>
+
+        <li>
+            <a href="../routes/logout.php" class="font-bold text-red-500">Sair da Conta</a>
+        </li>
+    </ul>
+</nav>
